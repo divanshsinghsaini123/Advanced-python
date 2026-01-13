51aea45cc2366a63d13d2f961f0a422069538646
Divansh Singh <divanshsinghsaini123@gmail.com>
2026-01-14 03:23:19 +0530

    added async await to middlewear ,important

--- ecommerse/app/main.py
+++ ecommerse/app/main.py
@@ -3,9 +3,9 @@ app = FastAPI()
 
 
 @app.middleware("http")
-def thisismiddlwear(request : request , call_next ):
+async def thisismiddlwear(request : request , call_next ):
     # yaha kutch bhi kro whatever you want 
-    response = call_next(request) #ab ye gya nest bnde ke pass jo bhi crud krne hain vo sb kro
+    response = await call_next(request) #ab ye gya nest bnde ke pass jo bhi crud krne hain vo sb kro
     # ab yaha se jane ka time ho gya 
     return response 
 @app.get("/")
