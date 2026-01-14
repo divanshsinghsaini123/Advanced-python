9f7e198caf788149683e9a439875b2dcb401584d
Divansh Singh <divanshsinghsaini123@gmail.com>
2026-01-15 00:46:28 +0530

    middlewear and decoratores

--- ecommerse/app/main.py
+++ ecommerse/app/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI , request 
 app = FastAPI()
 
+
+@app.middleware("http")
+def thisismiddlwear(request : request , call_next ):
+    # yaha kutch bhi kro whatever you want 
+    response = call_next(request) #ab ye gya nest bnde ke pass jo bhi crud krne hain vo sb kro
+    # ab yaha se jane ka time ho gya 
+    return response 
 @app.get("/")
 def temp():
     return {"message" : "whhehere"}
